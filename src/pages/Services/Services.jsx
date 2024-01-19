f697a1f0b698058c0b0c7b70083a8d9351238c82
Rahmat ullah badsha <rahmatur02982@gmail.com>
2024-01-19 23:45:04 +0600

    hjkhjk

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,8 +1,4 @@
-import HeroAbout from "../../component/AboutComponents/HeroAbout/HeroAbout";
-import OurMission from "../../component/AboutComponents/OurMission/OurMission";
-import MarketingTeam from "../../component/AboutComponents/MarketingTeam/MarketingTeam";
-import LeadershipTeam from "../../component/AboutComponents/LeadershipTeam/LeadershipTeam";
-import JoinOurTeam from "../../component/AboutComponents/JoinOurTeam/JoinOurTeam";
+
 import Service from "../../component/service/Service";
 import RequestSubmit from "../../shared/RequestSubmit/RequestSubmit";
 
